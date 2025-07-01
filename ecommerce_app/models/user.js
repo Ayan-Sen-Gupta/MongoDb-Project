f1feeff322c46263a2e3672230026e36cf0c0f75
Ayan-Sen-Gupta <ayansengupta560@gmail.com>
2025-07-01 11:29:18 +0530

    Order System

--- ecommerce_app/models/user.js
+++ ecommerce_app/models/user.js
@@ -1,6 +1,3 @@
-const mongodb = require('mongodb');
-const getDb = require('../util/database').getDb;
-
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
@@ -52,11 +49,15 @@ userSchema.methods.addToCart = function(product) {
 };
 
 userSchema.methods.removeFromCart = function(productId) {
-  
   const updatedCartItems = this.cart.items.filter(item => {
     return item.productId.toString() !== productId.toString();
   });
   this.cart.items = updatedCartItems;
+  return this.save();
+};
+
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
   return this.save();
 };
 
